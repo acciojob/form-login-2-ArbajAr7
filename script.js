a3504594b259c1eedc4313bc858acaa6be09cc91
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-12 23:22:06 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function show_alert() {
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
+	let pnumber = document.getElementById("pnumber").value;
+	let emailid = document.getElementById("emailid").value;
+	alert("First Name: "+fname+" Last Name: "+lname+" Phone Number: "+pnumber+" Email ID: "+emailid);
+	return false;
+}
+
+let alertdialogue = document.getElementById("loginForm");
+alertdialogue.addEventListener("submit",show_alert);
